759ea4bda68f0c560fc0a6d3dee896dff4d17732
Tania Holst <tania.holst@nav.no>
2019-11-01 11:25:47 +0100

    #11 updated dates

--- src/pages/Frontpage/Frontpage.tsx
+++ src/pages/Frontpage/Frontpage.tsx
@@ -4,7 +4,7 @@ import { Jumbotron } from '../../components/Jumbotron/Jumbotron';
 export function FrontPage() {
     return (
         <>
-            <Jumbotron title="JavaZone 2019" subTitle="September 11th - 12th" location="Oslo Spektrum" />
+            <Jumbotron title="JavaZone 2020" subTitle="September 9th - 10th" location="Oslo Spektrum" />
         </>
     )
 }
