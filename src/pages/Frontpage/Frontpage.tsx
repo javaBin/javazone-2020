379bfda162e92897368576e28c840a3812c354bc
alexander.svendsen <alexandersvendsen1990@gmail.com>
2020-08-05 23:10:17 +0200

    New frontpage info
    Move VR in focus

--- src/pages/Frontpage/Frontpage.tsx
+++ src/pages/Frontpage/Frontpage.tsx
@@ -4,10 +4,10 @@ import { Jumbotron } from '../../components/Jumbotron/Jumbotron';
 export function FrontPage() {
     return (
         <>
-            <Jumbotron title="JavaZone 2020"
-                       subTitle="has been moved to Jan 20th - 21th 2021"
-                       subsubTitle="But there will be a JavaZone VR on Sept 9th 2020"
-                       location="Oslo Spektrum" />
+            <Jumbotron title="JavaZone VR"
+                       subTitle="Sept 9th 2020"
+                       subsubTitle="There will be a regular JavaZone Jan 20th - 21th 2021"
+                       location="Online" />
         </>
     )
 }
