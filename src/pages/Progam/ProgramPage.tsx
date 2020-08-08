31ad8581f0340db7b2045791c1b6199b2da74cb0
Anders Karlsen <karlsenanders@gmail.com>
2020-08-08 07:48:31 +0200

    Added some info on vr track

--- src/pages/Progam/ProgramPage.tsx
+++ src/pages/Progam/ProgramPage.tsx
@@ -12,8 +12,14 @@ export function ProgramPage(){
             <Seperator />
             <CenterSection header={<h1>Information</h1>}>
                 <p>
-                    Some information about how the online session will work will be here
+                    JavaZone VR will have four parallell tracks, and you can follow them stream lived here the whole day.  
                 </p>
+                <ul>
+                    <li>We will host a live track from Teknologihuset in Oslo.</li>
+                    <li>The second track will be at our partners NAV in Oslo.</li>
+                    <li>The third track will be live from our partners Kantega in Trondheim</li>
+                    <li>The final track will be a online track from speakers from all over the world</li>
+                </ul>
                 <p>
                     See below for a detailed program. And watch this space for updates on how to watch them live!
                 </p>
