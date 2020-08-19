201c3045ad6f99fbb4ce7061da0e49022482f438
Rafael Winterhalter <rafael.wth@gmail.com>
2020-08-19 21:26:42 +0200

    Fix keynote title.

--- src/core/data/Program.data.ts
+++ src/core/data/Program.data.ts
@@ -7,7 +7,7 @@ export const program: Program[] = [
             {
                 time: '9.30 – 10.30',
                 talks: [{
-                    title: 'Keynote: Loom',
+                    title: 'Keynote: Scalable, Harmonious Concurrency for the Java Platform',
                     speakers: 'Ron Pressler',
                     abstract: 'Concurrent applications, those serving multiple independent application actions simultaneously, are the bread and butter of Java server-side programming. The thread has been Java’s primary unit of concurrency since Java’s inception, and is a core construct around which the entire Java platform is designed, but its cost is such that it can no longer efficiently represent a domain unit of concurrency, such as the session, request or transaction. As a result, Java has seen a proliferation of libraries and frameworks that offer scalability at the cost of abandoning the thread as the unit of software concurrency and, with it, the straightforward support of Java’s observability tooling, such as debuggers and profilers. Project Loom aims to reinstate the thread as an efficient unit of concurrency by adding a lightweight implementation of threads to the Java platform, which would allow straightforward code — that’s easy to write, understand and maintain, and works in harmony with the platform and its tooling — to scale and meet the requirements of even most demanding concurrent applications.'
 
@@ -86,7 +86,7 @@ export const program: Program[] = [
             {
                 time: '9.30 – 10.30',
                 talks: [{
-                    title: 'Keynote: Loom',
+                    title: 'Keynote: Scalable, Harmonious Concurrency for the Java Platform',
                     speakers: 'Ron Pressler',
                     abstract: 'Concurrent applications, those serving multiple independent application actions simultaneously, are the bread and butter of Java server-side programming. The thread has been Java’s primary unit of concurrency since Java’s inception, and is a core construct around which the entire Java platform is designed, but its cost is such that it can no longer efficiently represent a domain unit of concurrency, such as the session, request or transaction. As a result, Java has seen a proliferation of libraries and frameworks that offer scalability at the cost of abandoning the thread as the unit of software concurrency and, with it, the straightforward support of Java’s observability tooling, such as debuggers and profilers. Project Loom aims to reinstate the thread as an efficient unit of concurrency by adding a lightweight implementation of threads to the Java platform, which would allow straightforward code — that’s easy to write, understand and maintain, and works in harmony with the platform and its tooling — to scale and meet the requirements of even most demanding concurrent applications.'
                 }]
@@ -165,7 +165,7 @@ export const program: Program[] = [
             {
                 time: '9.30 – 10.30',
                 talks: [{
-                    title: 'Keynote: Loom',
+                    title: 'Keynote: Scalable, Harmonious Concurrency for the Java Platform',
                     speakers: 'Ron Pressler',
                     abstract: 'Concurrent applications, those serving multiple independent application actions simultaneously, are the bread and butter of Java server-side programming. The thread has been Java’s primary unit of concurrency since Java’s inception, and is a core construct around which the entire Java platform is designed, but its cost is such that it can no longer efficiently represent a domain unit of concurrency, such as the session, request or transaction. As a result, Java has seen a proliferation of libraries and frameworks that offer scalability at the cost of abandoning the thread as the unit of software concurrency and, with it, the straightforward support of Java’s observability tooling, such as debuggers and profilers. Project Loom aims to reinstate the thread as an efficient unit of concurrency by adding a lightweight implementation of threads to the Java platform, which would allow straightforward code — that’s easy to write, understand and maintain, and works in harmony with the platform and its tooling — to scale and meet the requirements of even most demanding concurrent applications.'
                 }]
@@ -234,7 +234,7 @@ export const program: Program[] = [
             {
                 time: '9.30 – 10.30',
                 talks: [{
-                    title: 'Keynote: Loom',
+                    title: 'Keynote: Scalable, Harmonious Concurrency for the Java Platform',
                     speakers: 'Ron Pressler',
                     abstract: 'Concurrent applications, those serving multiple independent application actions simultaneously, are the bread and butter of Java server-side programming. The thread has been Java’s primary unit of concurrency since Java’s inception, and is a core construct around which the entire Java platform is designed, but its cost is such that it can no longer efficiently represent a domain unit of concurrency, such as the session, request or transaction. As a result, Java has seen a proliferation of libraries and frameworks that offer scalability at the cost of abandoning the thread as the unit of software concurrency and, with it, the straightforward support of Java’s observability tooling, such as debuggers and profilers. Project Loom aims to reinstate the thread as an efficient unit of concurrency by adding a lightweight implementation of threads to the Java platform, which would allow straightforward code — that’s easy to write, understand and maintain, and works in harmony with the platform and its tooling — to scale and meet the requirements of even most demanding concurrent applications.'
                 }]
