7cfa17872a1a9ba7320ac4a2418f726441262c74
Tommy Hamarsnes <tommyhamarsnes@gmail.com>
2020-08-22 13:19:59 +0200

    Resolved the unresolved

--- src/core/models/Program.model.ts
+++ src/core/models/Program.model.ts
@@ -7,12 +7,8 @@ export type Speakers = Speaker[]
 
 export interface Talk {
     title: string;
-<<<<<<< HEAD
-    id: string,
-    speakers?: string;
-=======
+    id: string;
     speakers?: Speakers;
->>>>>>> edc7743a48d4d3895fb87d3716fca444ead90bb9
     abstract?: string;
 }
 
